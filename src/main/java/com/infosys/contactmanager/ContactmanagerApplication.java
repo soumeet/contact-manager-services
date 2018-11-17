e1a298667e85c7be0cbdddd896fd5f7e5636a3ed
Soumeet <soumeet.basak@gmail.com>
2018-11-18 00:02:24 +0530

    initial commit
    initializing web services for contact manager

--- src/main/java/com/infosys/contactmanager/ContactmanagerApplication.java
+++ src/main/java/com/infosys/contactmanager/ContactmanagerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactmanagerApplication {
+public class ContactManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactmanagerApplication.class, args);
+		SpringApplication.run(ContactManagerApplication.class, args);
 	}
 }
